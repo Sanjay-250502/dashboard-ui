147556636d2ec654956f69e65d9586ec69de8f8a
Sanjay-250502 <sanjayramachandran2002@gmail.com>
2023-09-26 22:07:20 +0530

    dashboard updated

---
+++ src/Data/TableData.js
@@ -0,0 +1,67 @@
+const TableData = [
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    }, 
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    },
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    },
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    },
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    },
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    },
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    },
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    },
+    {
+        InvoiceID: "#AHGA68",
+        Date: "23/09/2022",
+        Customer: "Jacob Marcus",
+        PayableAmount: "100",
+        PaidAmount: "000",
+    }
+]
+
+export default TableData;
